7234b53cf578f958488be6b6c1bd9c9062581a38
thanaphon <thanaphon_bo@kkumail.com>
2023-05-05 17:05:03 +0700

    Test main

--- src/test/java/com/Boonprakob/Thanaphon/AppTest.java
+++ src/test/java/com/Boonprakob/Thanaphon/AppTest.java
@@ -3,7 +3,6 @@ package com.Boonprakob.Thanaphon;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
@@ -16,5 +15,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void testMethod()
+    {
+        String s[] = {"First String","second string"};
+        App.main(s);
     }
 }
