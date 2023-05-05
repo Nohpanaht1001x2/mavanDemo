0b6b23bf568b952185a9b888a7fd7016149ac89f
thanaphon <thanaphon_bo@kkumail.com>
2023-05-05 18:19:59 +0700

    write with absolute path test (not compatible with another machine yet.)

--- src/main/java/com/Boonprakob/Thanaphon/App.java
+++ src/main/java/com/Boonprakob/Thanaphon/App.java
@@ -1,5 +1,7 @@
 package com.Boonprakob.Thanaphon;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,12 +13,20 @@ public class App {
     {
         System.out.println("Hello World!");
         System.out.println("Hello World! but second line!");
-        FileWriter fileWriter = new FileWriter("text.txt");
+        File file = new File("/home/thanaphon/text.txt");
+        FileWriter fileWriter = new FileWriter(file);
         for (String s:args)
         {
             fileWriter.write(s+"\n");
         }
-        fileWriter.write("กาก้าวไกล กาๆไปเหอะ");
+        fileWriter.write(String.format("%s%n","กาก้าวไกล กาๆไปเหอะ" ));
+        fileWriter.flush();
         fileWriter.close();
+        FileReader fileReader = new FileReader(file);
+        char[] c = new char[(int)file.length()-36]; // hard-code, will fix later
+        fileReader.read(c);
+        System.out.println(file.length());
+        System.out.println(c);
+        fileReader.close();
     }
 }
