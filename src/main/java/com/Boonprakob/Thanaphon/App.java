0f80f5f63ba65d9a9d957a165d9e2cfaacb1d00d
Nohpanaht1001x2 <thanaphon_bo@kkumail.com>
2023-05-07 23:53:32 +0700

    add try catch block

--- src/main/java/com/Boonprakob/Thanaphon/App.java
+++ src/main/java/com/Boonprakob/Thanaphon/App.java
@@ -13,20 +13,32 @@ public class App {
     {
         System.out.println("Hello World!");
         System.out.println("Hello World! but second line!");
-        File file = new File("/home/thanaphon/text.txt");
-        FileWriter fileWriter = new FileWriter(file);
-        for (String s:args)
+        // File file = new File("/home/thanaphon/text.txt");   //absolute path, depend on local machine   
+        String fileName = new String();
+        fileName = "text.txt" ;
+        File file = new File(fileName);  //linux   
+        // File file = new File(System.getProperty("user.dir", "/"+fileName));  //windows      
+        System.out.println(file.getAbsolutePath());
+        try (FileWriter fileWriter = new FileWriter(file);FileReader fileReader = new FileReader(file);)
         {
-            fileWriter.write(s+"\n");
+            for (String s:args)
+            {
+                fileWriter.write(s+"\n");
+            }
+            fileWriter.write(String.format("%s%n","กาก้าวไกล กาๆไปเหอะ" ));
+            fileWriter.flush();
+            // fileWriter.close(); //close does not require since use try-with-resource with AutoCloseable
+            
+            char[] c = new char[(int)file.length()-36]; // hard-code, will fix later
+            fileReader.read(c);
+            System.out.println(file.length());
+            System.out.println(c);
+            // fileReader.close(); //close does not require since use try-with-resource with AutoCloseable
         }
-        fileWriter.write(String.format("%s%n","กาก้าวไกล กาๆไปเหอะ" ));
-        fileWriter.flush();
-        fileWriter.close();
-        FileReader fileReader = new FileReader(file);
-        char[] c = new char[(int)file.length()-36]; // hard-code, will fix later
-        fileReader.read(c);
-        System.out.println(file.length());
-        System.out.println(c);
-        fileReader.close();
+        catch(Exception e)
+        {
+            System.out.println("there is a exception happen"+"\n"+e.getMessage());
+        }
+        
     }
 }
