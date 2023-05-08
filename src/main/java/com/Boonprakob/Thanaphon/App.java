619d0fbab4a75d2974ed9b7fe428befcb3ec7ff9
thanaphon <thanaphon_bo@kkumail.com>
2023-05-08 10:47:04 +0700

    handle fileReader and write some comment

--- src/main/java/com/Boonprakob/Thanaphon/App.java
+++ src/main/java/com/Boonprakob/Thanaphon/App.java
@@ -8,37 +8,41 @@ import java.io.IOException;
 /**
  * Hello world!
  */
-public class App {
+public class App
+{
+
     public static void main(String[] args) throws IOException
     {
         System.out.println("Hello World!");
         System.out.println("Hello World! but second line!");
-        // File file = new File("/home/thanaphon/text.txt");   //absolute path, depend on local machine   
-        String fileName = new String();
-        fileName = "text.txt" ;
-        File file = new File(fileName);  //linux   
+        // File file = new File("/home/thanaphon/text.txt");   //absolute path, depend on local machine
+        String fileName = "text.txt";
+        File file = new File(fileName);  //linux
         // File file = new File(System.getProperty("user.dir", "/"+fileName));  //windows      
         System.out.println(file.getAbsolutePath());
-        try (FileWriter fileWriter = new FileWriter(file);FileReader fileReader = new FileReader(file);)
+        try (FileWriter fileWriter = new FileWriter(file); FileReader fileReader = new FileReader(file))
         {
-            for (String s:args)
+            for (String s : args)
             {
-                fileWriter.write(s+"\n");
+                fileWriter.write(s + "\n");
             }
-            fileWriter.write(String.format("%s%n","กาก้าวไกล กาๆไปเหอะ" ));
-            fileWriter.flush();
-            // fileWriter.close(); //close does not require since use try-with-resource with AutoCloseable
-            
-            char[] c = new char[(int)file.length()-36]; // hard-code, will fix later
-            fileReader.read(c);
+            fileWriter.write(String.format("%s%n", "กาก้าวไกล กาๆไปเหอะ"));
+            fileWriter.flush();     //write String buffer to file
+//            fileWriter.close();     //close does not require since use try-with-resource with AutoCloseable
+
+            char[] c = new char[(int) file.length() - 36];      // hard-code, will fix later
+            if(fileReader.read(c) == -1)        //already execute in if argument
+            {
+                throw new IOException();
+            }
             System.out.println(file.length());
             System.out.println(c);
-            // fileReader.close(); //close does not require since use try-with-resource with AutoCloseable
+//            fileReader.close();     //close does not require since use try-with-resource with AutoCloseable
         }
-        catch(Exception e)
+        catch (Exception e)
         {
-            System.out.println("there is a exception happen"+"\n"+e.getMessage());
+            System.out.println("there is a exception happen" + "\n" + e.getMessage());
         }
-        
+
     }
 }
